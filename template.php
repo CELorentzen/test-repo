31a1340ce1d3cc989df62cb6db52c919eee3d27e
CELorentzen <73616170+CELorentzen@users.noreply.github.com>
2020-11-25 12:30:11 +0100

    WP_Query added to template
    This WP_Query is the one that finds the entire list of co-workers from the CPT

--- template.php
+++ template.php
@@ -14,6 +14,33 @@ include 'header.php';
         endwhile;
         ?>
 
+        <?php
+        $args = array(
+          'post_type'       => 'medarbejder',
+          'posts_per_page'  => -1,
+          'category'        => '',
+        );
+
+        $workQuery = new WP_Query ( $args );
+
+        if ($workQuery->have_posts() ){
+            echo '<ul>';
+            while ($workQuery->have_posts() ){
+              $workQuery->the_post();
+              echo '<li>' . get_the_title() . '</li>';
+              echo '<p>' . get_the_post_thumbnail( get_the_ID(), 'full') . '</p>';
+              echo '<p>' . get_field('stilling', $postID, false) . '</p>';
+              echo '<p>' . get_field('telefon', $postID, false) . '</p>';
+            }
+            echo '</ul>';
+        } else {
+          //If CPT is empty
+          echo 'Ingen medarbejdere ikke fundet';
+        }
+        wp_reset_postdata();
+
+        ?>
+
     </main>
 
 
