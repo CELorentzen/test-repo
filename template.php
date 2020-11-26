6290fb68462f040410fac958b56b1bd9a2173772
CELorentzen <73616170+CELorentzen@users.noreply.github.com>
2020-11-26 16:02:51 +0100

    Altered Loop function
    Altered Loop function to sort the CPT by category

--- template.php
+++ template.php
@@ -22,11 +22,11 @@ get_header();
 
         <?php 
             $option = '<option value="all">Alle Medarbejdere</option>'; // Change page to default
-            $categories = get_categories(); 
-            foreach ($categories as $category) {
-              $option .= '<option value="'.$category->slug.'">';
-              $option .= $category->cat_name;
-              $option .= ' ('.$category->category_count.')';
+            $afdelinger = get_categories(); 
+            foreach ($afdelinger as $afdeling) {
+              $option .= '<option value="'.$afdeling->slug.'">';
+              $option .= $afdeling->cat_name;
+              $option .= ' ('.$afdeling->category_count.')';
               $option .= '</option>';
             }
             echo $option;
@@ -36,40 +36,86 @@ get_header();
 
         <?php
 
-        $selAfd = sanitize_text_field( get_query_var('afdeling')); //check URL for appended data
-        
-        $args = array(
-          'post_type'       => 'medarbejder',
-          'posts_per_page'  => -1,
-          'order'           => 'ASC',
-          'tax_query'       => array(
-            array(
-              'taxonomy'      => 'category',
-              'field'         => 'name',
-              'terms'         => $selAfd,
-            ),
-          ),
-        );
+        $afdelingSelection = sanitize_text_field( get_query_var('afdeling')); //check URL for appended data
+       
+        if (isset($afdelingSelection)){
+          
+          //If a specific department is selected
+          if ($afdelingSelection != 'all' && $afdelingSelection != ''){
+            $args = array(
+              'post_type'       => 'medarbejder',
+              'posts_per_page'  => -1,
+              'order'           => 'ASC',
+              'tax_query'       => array(
+                array(
+                  'taxonomy'      => 'category',
+                  'field'         => 'name',
+                  'terms'         => $afdelingSelection,
+                ),
+              ),
+            );
+            
+            //run The Loop to get each post
+            $workQuery = new WP_Query ( $args );
 
-
-        $workQuery = new WP_Query ( $args );
-
-        if ($workQuery->have_posts() ){
-            echo '<ul>';
-            while ($workQuery->have_posts() ){
-              $workQuery->the_post();
-              echo '<li>' . get_the_title() . '</li>';
-              echo '<p>' . get_the_post_thumbnail( get_the_ID(), 'full') . '</p>';
-              echo '<p>' . get_field('stilling', $postID, false) . '</p>';
-              echo '<p>' . get_field('telefon', $postID, false) . '</p>';
+            if ($workQuery->have_posts() ){
+                echo '<ul>';
+                while ($workQuery->have_posts() ){
+                  $workQuery->the_post();
+                  echo '<li>' . get_the_title() . '</li>';
+                  echo '<p>' . get_the_post_thumbnail( get_the_ID(), 'full') . '</p>';
+                  echo '<p>' . get_field('stilling', $postID, false) . '</p>';
+                  echo '<p>' . get_field('telefon', $postID, false) . '</p>';
+                }
+                echo '</ul>';
+            } else {
+              //If CPT is empty
+              echo 'Ingen medarbejdere ikke fundet';
             }
-            echo '</ul>';
-        } else {
-          //If CPT is empty
-          echo 'Ingen medarbejdere ikke fundet';
-        }
-        wp_reset_postdata();
+            wp_reset_postdata();
 
+          }
+
+          //If no department is selected, or the All option is selected
+          else if ($afdelingSelection == 'all' || $afdelingSelection == '') {
+
+            //Loop to sort the list by department
+            $afdelingLoop = get_categories(); 
+            foreach ($afdelingLoop as $currentAfdeling) {
+              $args = array(
+                'post_type'       => 'medarbejder',
+                'posts_per_page'  => -1,
+                'order'           => 'ASC',
+                'tax_query'       => array(
+                  array(
+                    'taxonomy'      => 'category',
+                    'field'         => 'name',
+                    'terms'         => $currentAfdeling,
+                  ),
+                ),
+              );
+              
+              //run The Loop to get each post for each category
+              $workQuery = new WP_Query ( $args );
+
+              if ($workQuery->have_posts() ){
+                  echo '<ul>';
+                  while ($workQuery->have_posts() ){
+                    $workQuery->the_post();
+                    echo '<li>' . get_the_title() . '</li>';
+                    echo '<p>' . get_the_post_thumbnail( get_the_ID(), 'full') . '</p>';
+                    echo '<p>' . get_field('stilling', $postID, false) . '</p>';
+                    echo '<p>' . get_field('telefon', $postID, false) . '</p>';
+                  }
+                  echo '</ul>';
+              } else {
+                //If CPT is empty
+                echo 'Ingen medarbejdere ikke fundet';
+              }
+              wp_reset_postdata();
+            }
+        }
+      }
         ?>
 
     </main>
