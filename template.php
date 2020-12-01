d219d27cb37a1213780aa865e8f74071e513e639
CELorentzen <73616170+CELorentzen@users.noreply.github.com>
2020-12-01 20:02:31 +0100

    Hardcode changed to dynamic
    The information fields at the start of each department and at the bottrom of the screen all had hardcoded their information. I changed them to be dynamic.

--- template.php
+++ template.php
@@ -65,10 +65,10 @@ get_header();
               echo '<div class="afdelingInfoBox">';
               echo '<div class="infoAfdeling infoHead">' . ucfirst($afdelingSelection) . '</div>'; //Department name taken from URL append. First letter is capitalized
               echo '<div class="infoTextContainer">';
-              echo '<div class="infoFirma infoText"> OnlinePlus </div>';
-              echo '<div class="infoAddress infoText"> Rugårdsvej 130D </div>';
-              echo '<div class="infoAddress infoText"> 5000 Odense </div>';
-              echo '<div class="infoPhone infoText"> Tlf: +45 70 22 11 44 </div>';
+              echo '<div class="infoFirma infoText">' . get_field('firma') . '</div>';
+              echo '<div class="infoAddress infoText">' . get_field('firmaAddress') . '</div>';
+              echo '<div class="infoAddress infoText">' . get_field('firmaCity') . '</div>';
+              echo '<div class="infoPhone infoText"> Tlf: +45 '. substr(get_field('firmaPhone'),0,2) . " " . substr(get_field('firmaPhone'),2,2) . " " .  substr(get_field('firmaPhone'),4,2) . " " . substr(get_field('firmaPhone'),6,2) . '</div>'; //The substrings split the phone number into groups of 2 digits
               echo '</div>';
             echo '</div>';
                   while ($workQuery->have_posts() ){
@@ -124,10 +124,10 @@ get_header();
                     echo '<div class="afdelingInfoBox">';
                       echo '<div class="infoAfdeling infoHead">' . $currentAfdeling->name . '</div>';
                       echo '<div class="infoTextContainer">';
-                      echo '<div class="infoFirma infoText"> OnlinePlus </div>';
-                      echo '<div class="infoAddress infoText"> Rugårdsvej 130D </div>';
-                      echo '<div class="infoAddress infoText"> 5000 Odense </div>';
-                      echo '<div class="infoPhone infoText"> Tlf: +45 70 22 11 44 </div>';
+                      echo '<div class="infoFirma infoText">' . get_field('firma') . '</div>';
+                      echo '<div class="infoAddress infoText">' . get_field('firmaAddress') . '</div>';
+                      echo '<div class="infoAddress infoText">' . get_field('firmaCity') . '</div>';
+                      echo '<div class="infoPhone infoText"> Tlf: +45 '. substr(get_field('firmaPhone'),0,2) . " " . substr(get_field('firmaPhone'),2,2) . " " .  substr(get_field('firmaPhone'),4,2) . " " . substr(get_field('firmaPhone'),6,2) . '</div>'; //The substrings split the phone number into groups of 2 digits
                       echo '</div>';
                     echo '</div>';
                     while ($workQuery->have_posts() ){
@@ -167,13 +167,13 @@ get_header();
                   echo '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-telephone-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">';
                     echo '<path fill-rule="evenodd" d="M2.267.98a1.636 1.636 0 0 1 2.448.152l1.681 2.162c.309.396.418.913.296 1.4l-.513 2.053a.636.636 0 0 0 .167.604L8.65 9.654a.636.636 0 0 0 .604.167l2.052-.513a1.636 1.636 0 0 1 1.401.296l2.162 1.681c.777.604.849 1.753.153 2.448l-.97.97c-.693.693-1.73.998-2.697.658a17.47 17.47 0 0 1-6.571-4.144A17.47 17.47 0 0 1 .639 4.646c-.34-.967-.035-2.004.658-2.698l.97-.969z"/>';
                   echo '</svg>';
-                  echo '+45 70 22 11 44';
+                  echo '+45 ' . substr(get_field('firmaPhone'),0,2) . " " . substr(get_field('firmaPhone'),2,2) . " " .  substr(get_field('firmaPhone'),4,2) . " " . substr(get_field('firmaPhone'),6,2);
                 echo '</div>';
                 echo '<div class="footerContact">';
                   echo '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-envelope" fill="currentColor" xmlns="http://www.w3.org/2000/svg">';
                     echo '<path fill-rule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>';
                   echo '</svg>';
-                  echo 'support@onlineplus.dk';
+                  echo get_field('firmaMail');
                 echo '</div>';
               echo '</div>';
             echo '</div>';
